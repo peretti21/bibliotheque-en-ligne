f1928ef676508635c13f90491e1fc5e85f9ca364
peretti21 <princesseveronne@yahoo.fr>
2020-06-20 04:23:17 +0200

    Commit initial : création du projet Sring Boot et configuration du fichier application.properties.

---
+++ bibliothequeenligne/src/main/java/com/code/bibliothequeenligne/BibliothequeenligneApplication.java
@@ -0,0 +1,13 @@
+package com.code.bibliothequeenligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeenligneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeenligneApplication.class, args);
+	}
+
+}
---
+++ bibliothequeenligne/src/test/java/com/code/bibliothequeenligne/BibliothequeenligneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.bibliothequeenligne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeenligneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
